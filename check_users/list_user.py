50de000228f6a195b7137d1fb95443aff2eec66d
David Rotermund <davrot@uni-bremen.de>
2024-07-16 18:58:16 +0200

    Create list_user.py

---
+++ check_users/list_user.py
@@ -0,0 +1,17 @@
+import pymongo
+
+container_name:str = "overleafmongo"
+port: int = 27017
+
+client = pymongo.MongoClient(container_name, port)
+db = client.sharelatex
+users = db.users
+
+cursor = users.find()
+
+for user in cursor:
+    print(user['email'])
+
+client.close()
+
+
