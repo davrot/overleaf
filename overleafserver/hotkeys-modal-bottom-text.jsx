e3abde02719f97687dfa974877d7afef4af136e0
David Rotermund <davrot@uni-bremen.de>
2024-07-21 02:15:42 +0200

    Create hotkeys-modal-bottom-text.jsx

---
+++ overleafserver/hotkeys-modal-bottom-text.jsx
@@ -0,0 +1,20 @@
+import { Trans } from 'react-i18next'
+import * as eventTracking from '../../../infrastructure/event-tracking'
+
+export default function HotkeysModalBottomText() {
+  return (
+    <div className="hotkeys-modal-bottom-text">
+      <Trans
+        i18nKey="a_more_comprehensive_list_of_keyboard_shortcuts"
+        components={[
+          // eslint-disable-next-line jsx-a11y/anchor-has-content, react/jsx-key
+          <a
+            onClick={() => eventTracking.sendMB('left-menu-hotkeys-template')}
+            href="https://www.overleaf.com/latex/templates/overleaf-keyboard-shortcuts/pphdnzrwmttk"
+            target="_blank"
+          />,
+        ]}
+      />
+    </div>
+  )
+}
